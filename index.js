3480c4269541a094d484234659bc0dba4b08e8ea
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-05-12 23:35:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ blaki.on('ready', async () => {
     
     const guild = blaki.guilds.get('577167959047405588');
     setInterval(function() {
-        	const HumansChannel = blaki.channels.get('577178212141105154');
+        	const HumansChannel = blaki.channels.find(`name`, "users");
 	        const OnlineChannel = blaki.channels.find(`name`, "online");
 		var HumansCount = guild.members.filter(m => !m.user.bot).size;
 	        var OnlineCount = guild.members.filter(o => o.presence.status === 'online').size;
