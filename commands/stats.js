e2ac80805e15ae0b430360a4c3e001a9995aa724
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-05-12 19:11:30 +0200

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (blaki, member, args) => {;
-	let humans = member.guild.members.filter(member => !member.user.blaki).size;
+	let humans = member.guild.members.filter(member => !member.user.bot).size;
 	member.guild.channels.get('577178212141105154').setName(`🔥 Jest Nas: ${humans}`)
 };
 
