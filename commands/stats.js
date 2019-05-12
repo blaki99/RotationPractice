3b17ffa537d9f614867445a88dae83aa3cf7a35a
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-05-12 19:09:59 +0200

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,12 +1,10 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (blaki, member, args) => {
-	let Szefuncio = message.guild.roles.find("name", "OWNER 👑");
-	if(!message.member.roles.has(Szefuncio.id)) return message.reply("oops");
+module.exports.run = async (blaki, member, args) => {;
 	let humans = member.guild.members.filter(member => !member.user.blaki).size;
 	member.guild.channels.get('577178212141105154').setName(`🔥 Jest Nas: ${humans}`)
 };
 
 module.exports.help = {
-    name: "stats"
+    name: "count"
 }
