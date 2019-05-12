d9cf0c612b858ffc9da46cfd05b61300fac07eea
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-05-12 20:51:10 +0200

    Update CustomsLate.js

--- commands/CustomsLate.js
+++ commands/CustomsLate.js
@@ -10,8 +10,8 @@ module.exports.run = async (blaki, message, args) => {
     const zasady2 = "**»** MNIEJ NIŻ 50 WALCZYMY OD POJAWIENIA SIĘ 4 STREFY !"
     const zasady3 = "**»** NIE BIJEMY LUDZI NA MIEJSCÓWKACH !"
     const zasady4 = "**»** NIE STRZELAMY W POJAZDY PRZED POJAWIENIEM SIĘ 3 LUB 4 STREFY !"
-    const ping = "<@&535100081444225035>"
-    const check = '577121399043522560'
+    const ping = "<@&577205221994922046>"
+    const check = '577203620144087040'
 
     if(!message.member.roles.has(Szefuncio.id)) return message.reply("oops");
     if(!args[0]) return message.channel.send("❌ _Wprowadź prawidłowe wartości, **b!ct hasło tryb**_ ❌").then(() =>
@@ -26,7 +26,7 @@ module.exports.run = async (blaki, message, args) => {
     .addField("Tryb Gry:", `**${mode}**`)
     .addField("Zasady:", zasady1 + `\n`+ zasady2 + `\n`+ zasady3 + `\n`+ zasady4)
     .setTimestamp(message.createdAt)
-    .setFooter("Kliknij reakcje jeśli grasz", "https://i.imgur.com/KyTy1HB.png");
+    .setFooter("Kliknij reakcje jeśli grasz", "https://i.imgur.com/HViVOMF.png");
     message.channel.send(ping);
     message.channel.send(customEmbed).then(function (message) {
         message.react(check)
